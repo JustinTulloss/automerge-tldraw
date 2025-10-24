b0d65dfc1efe7060ea9e79eb9dafd0761b5d806d
Justin Tulloss <justin.tulloss@gmail.com>
2025-10-24 12:35:38 -0700

    Updated default_store to actually produce a default store data structure

--- src/default_store.ts
+++ src/default_store.ts
@@ -1,4 +1,4 @@
-import { TLStoreSnapshot, createTLStore } from "tldraw"
+import { TLStoreSnapshot, createTLStore, loadSnapshot } from "tldraw"
 import { cloneSnapshot } from "./utils"
 
 let cachedDefaultSnapshot: TLStoreSnapshot | null = null
@@ -7,7 +7,8 @@ export function getDefaultStoreSnapshot(): TLStoreSnapshot {
   if (!cachedDefaultSnapshot) {
     const store = createTLStore({})
     const snapshot = store.getStoreSnapshot()
-    cachedDefaultSnapshot = snapshot;
+    loadSnapshot(store, snapshot)
+    cachedDefaultSnapshot = store.getStoreSnapshot()
   }
 
   return cloneSnapshot(cachedDefaultSnapshot!)
